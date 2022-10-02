d0da675affb885d702688e7585d71aabb167fdce
SyakirZaidi <zsyakir20@gmail.com>
2022-10-02 20:00:58 +0800

    menambah fungsi contact form

--- script.js
+++ script.js
@@ -104,3 +104,32 @@ scrollLinks.forEach((link) => {
     linksContainer.style.height = 0;
   });
 });
+
+// contact form function
+const scriptURL = "https://script.google.com/macros/s/AKfycbwYAmEQMKgJ2xQ6yXavBsEYCuY0k44JRETfHHeCui9XBCUcTvKJ_GXcmwLxibqpjjo/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+const btnSend = document.querySelector(".btn-send");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika btn send diklik
+  // tampilkan btn loading, hilangkan btn send
+  btnLoading.classList.toggle("d-none");
+  btnSend.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan btn send, hilangkan btn loading
+      btnLoading.classList.toggle("d-none");
+      btnSend.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
